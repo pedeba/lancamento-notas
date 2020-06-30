f3d282e762ab26c60502ac3cda6ea23f6c92c093
pedeba <pedropedeba@gmail.com>
2020-06-30 16:10:22 -0300

    notas

---
+++ script.js
@@ -0,0 +1,59 @@
+const turma = window.document.getElementById("turmas")
+const aluno01 = window.document.getElementById('aluno001')
+const aluno02 = window.document.getElementById('aluno002')
+const aluno03 = window.document.getElementById('aluno003')
+const aluno04 = window.document.getElementById('aluno004')
+const aluno05 = window.document.getElementById('aluno005')
+const nota01 = window.document.getElementById('nota1')
+const nota02 = window.document.getElementById('nota2')
+const nota03 = window.document.getElementById('nota3')
+const nota04 = window.document.getElementById('nota4')
+const nota05 = window.document.getElementById('nota5')
+const nota06 = window.document.getElementById('nota6')
+const nota07 = window.document.getElementById('nota7')
+const nota08 = window.document.getElementById('nota8')
+const nota09 = window.document.getElementById('nota9')
+const nota10 = window.document.getElementById('nota10')
+const table = window.document.getElementById("turmasecao")
+
+function turma1() {
+    table.classList.remove('some')
+    turma.innerHTML = 'turma 1'
+    aluno01.innerHTML = 'Florisvaldo'
+    aluno02.innerHTML = 'Samara'
+    aluno03.innerHTML = 'Pedro'
+    aluno04.innerHTML = 'Marcela'
+    aluno05.innerHTML = 'Julio'
+    nota01.innerHTML = '10,00'
+    nota02.innerHTML = '8,00'
+    nota03.innerHTML = '4,00'
+    nota04.innerHTML = '7,50'
+    nota05.innerHTML = '7,50'
+    nota06.innerHTML = '9,80'
+    nota07.innerHTML = '10,00'
+    nota08.innerHTML = '6,00'
+    nota09.innerHTML = '6,00'
+    nota10.innerHTML = '8,00'
+}
+
+function turma2() {
+    table.classList.remove('some')
+    turma.innerHTML = 'turma 2'
+    aluno01.innerHTML = 'Michel'
+    aluno02.innerHTML = 'Isabela'
+    aluno03.innerHTML = 'Igor'
+    aluno04.innerHTML = 'Marcelo'
+    aluno05.innerHTML = 'Amanda'
+    nota01.innerHTML = '5,00'
+    nota02.innerHTML = '8,00'
+    nota03.innerHTML = '9,00'
+    nota04.innerHTML = '7,50'
+    nota05.innerHTML = '8,00'
+    nota06.innerHTML = '3,00'
+    nota07.innerHTML = '7,00'
+    nota08.innerHTML = '7,50'
+    nota09.innerHTML = '8,00'
+    nota10.innerHTML = '10,00'
+}
+
+
